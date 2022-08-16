d79d16e9838f715c11fcb2e988851f66debbae4e
Ivan <ivan.cavlek1403@gmail.com>
2022-08-16 20:23:37 +0200

    Refactoring - Test SUCCESS

--- resources/DataPlotter.py
+++ resources/DataPlotter.py
@@ -10,18 +10,6 @@ class DataPlotter:
         plt.show()
 
     @staticmethod
-    def show_portfolios_scatter(returns, volatilities, xlabel=10, ylabel=6):
-        returns = np.array(returns)
-        volatilities = np.array(volatilities)
-        plt.figure(figsize=(10, 6))
-        plt.scatter(volatilities, returns, c=returns/volatilities, marker='o')
-        plt.grid(True)
-        plt.xlabel('Expected Volatility')
-        plt.ylabel('Expected Return')
-        plt.colorbar(label='Sharpe Ratio')
-        plt.show()
-
-    @staticmethod
     def show_portfolios_scatter_with_sharpe_ratio(opt, rets, portfolio_rets, portfolio_vols, val1, val2):
         plt.figure(figsize=(10, 6))
         plt.scatter(portfolio_vols, portfolio_rets, c=portfolio_rets / portfolio_vols, marker='o')
